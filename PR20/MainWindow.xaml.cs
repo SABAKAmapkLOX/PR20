ea2faf54380649211b6004e71a67d2adce03ec42
исп-34 <-34@LAB24-05>
2025-04-30 12:38:54 +0300

    Привет

--- PR20/MainWindow.xaml.cs
+++ PR20/MainWindow.xaml.cs
@@ -60,6 +60,16 @@ namespace PR20
                 case "Количество объектов в одном городе": LINQ(1); break;
                 case "Итоговой стоимости всех работ одного вида": LINQ(2); break;
             }
+
+            var a = sender as Button;
+            switch (a.Name)
+            {
+                case "btSQL1": LINQ(3); break;
+                case "btSQL2": LINQ(4); break;
+                case "btSQL3": LINQ(5); break;
+                case "btSQL4": LINQ(6); break;
+                case "btSQL5": LINQ(7); break;
+            }
         }
 
         private void AddItem()
@@ -142,6 +152,58 @@ namespace PR20
                                              ИтоговаяСтоимость = grouped.Sum(r => r.Price)
                                          });
                         dataGridSQL.ItemsSource = resultZp2.ToList();
+                        ; break;
+
+                    //просмотр сведений обо всех объектах, на которых предусмотрено выполнение работ одного вида
+                    case 3:
+                        MessageBox.Show("Нема");
+                        ; break;
+
+                    //добавление записей в таблицу Справочник окончания работ
+                    case 4:
+                        var newEndRecords = new List<DirectoryCompletionWork>
+                        {
+                            new DirectoryCompletionWork { IdObject = 55 ,DateCompletionDate = new DateTime(2025, 9, 30) },
+                            new DirectoryCompletionWork { IdObject = 56 ,DateCompletionDate = new DateTime(2025, 10, 15) },
+                            new DirectoryCompletionWork { IdObject = 57 ,DateCompletionDate = new DateTime(2025, 11, 1) }
+                        };
+                        _db.DirectoryCompletionWorks.AddRange(newEndRecords);
+                        _db.SaveChanges();
+                        dataGridSQL.ItemsSource = newEndRecords.ToList();
+                        ; break;
+
+                    //подсчет количества месяцев строительства
+                    case 5:
+                        MessageBox.Show("Нема");
+                        ; break;
+
+                    //вывести список работ, срок окончания которых предусмотрен в 3 квартале этого года
+                    case 6:
+                        var query4 = (from price in _db.DirectoryPrices
+                                      join volumeWorkObject in _db.VolumeWorkObjects
+                                        on price.IdWork equals volumeWorkObject.IdWork
+                                      join compWorks in _db.DirectoryCompletionWorks
+                                        on volumeWorkObject.IdObject equals compWorks.IdObject
+                                      where compWorks.DateCompletionDate >= new DateTime(2025, 7, 1) &&
+                                      compWorks.DateCompletionDate <= new DateTime(2025, 9, 30)
+                                      select new
+                                      {
+                                          НаименованиеРаботы = price.NameWork
+                                      });
+                        dataGridSQL.ItemsSource = query4.ToList();
+                        ; break;
+
+                    //вывести Наименование работ с максимальной расценкой
+                    case 7:
+                        var query5 = (from price in _db.DirectoryPrices
+                                      join volumeWorkObject in _db.VolumeWorkObjects
+                                      on price.IdWork equals volumeWorkObject.IdWork
+                                      select new
+                                      { 
+                                          НаименованиеРаботы = price.NameWork,
+                                          МаксимальнаяРасценка = _db.DirectoryPrices.Max(p => p.Price)
+                                      });
+                        dataGridSQL.ItemsSource = query5.ToList();
                         ; break;
                 }
             }
